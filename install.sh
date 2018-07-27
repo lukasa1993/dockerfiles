55263f63042f5dab980043b06fb5082979797e6b
luka dodelia <luka@picktek.com>
2018-07-27 10:59:36 +0400

    adding healthcheck

--- install.sh
+++ install.sh
@@ -15,11 +15,10 @@ pip install awscli
 apk del py-pip
 
 # install go-cron
-apk add curl
-curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
+wget https://github.com/lukasa1993/go-cron/releases/download/v0.0.7/go-cron-linux.gz
+gunzip go-cron-linux.gz
+mv ./go-cron-linux /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
-apk del curl
-
 
 # cleanup
 rm -rf /var/cache/apk/*
